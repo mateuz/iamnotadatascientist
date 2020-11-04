59a22b06606d5af8c3eb0710380fe193ef8f51e3
Mateus Boiani <mateuz.boiani@gmail.com>
2020-11-04 20:38:51 -0300

    testing RBF SVM

--- main.py
+++ main.py
@@ -1,80 +1,112 @@
 import numpy as np   # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+
 from sklearn import preprocessing
+from sklearn.model_selection import train_test_split, cross_val_predict, GridSearchCV
+from sklearn import svm
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score, f1_score, recall_score
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import chart_studio.plotly as py
+from plotly.offline import iplot #init_notebook_mode
+#init_notebook_mode(connected=True)
+import plotly.graph_objs as go
+from plotly import tools
+
+def tuning():
+
+  # Params
+  # C - contrabalança a classificação incorreta de exemplos
+  # de treinamento pela simplicidade da superfície de decisão.
+  # Um C baixo torna a superfície de decisão suave, enquanto 
+  # um C alto visa classificar todos os exemplos de treinamento
+  # corretamente
+  # 
+  # Gamma - define quanta influência um único exemplo de 
+  # treinamento tem. Quanto maior for a gama, mais próximos 
+  # os outros exemplos devem estar para serem afetados.
+  #
+  # 1.4.6.1. Parameters of the RBF Kernel
+  # https://scikit-learn.org/stable/modules/svm.html#tips-on-practical-use
+  # Proper choice of C and gamma is critical to the SVM’s performance.
+  # One is advised to use sklearn.model_selection.GridSearchCV with 
+  # C and gamma spaced exponentially far apart to choose good values.
+
+  df = pd.read_csv("data/mushrooms.csv")
+
+  features = df.select_dtypes(include="object").columns
+
+  encoder = preprocessing.LabelEncoder() 
+
+  for feat in features:
+    df[feat] = encoder.fit_transform(df[feat].astype(str))
+
+  X = df.drop(['class', 'stalk-root'], axis = 1)
+  Y = df['class']
+
+  (X_train, X_test, y_train, y_test) = train_test_split(X, Y, test_size = 0.20, random_state = 25)
+
+  clf = svm.SVC()
+  param_grid = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4, 1e-5],'C': [1, 10, 25, 50, 75, 100]}, {'kernel': ['linear'], 'C': [1, 10, 25, 50, 100]}]
+  grid = GridSearchCV(clf, param_grid, cv = 10, scoring = 'accuracy')
+
+  print("Tuning hyper-parameters")
+  grid.fit(X_train,y_train)
+  print(grid.best_params_)
+  print(np.round(grid.best_score_,3))
 
 def main():
-
   # read data file
-  df = pd.read_csv("data\mushrooms.csv")
-  print(df)
-
-  # read first 6 rows
+  df = pd.read_csv("data/mushrooms.csv")
+  
   df.head(6)
-
-  # read last 6 rows
   df.tail(6)
 
-  # read structure of dataframe
-  # df in which 8124 rows,23 columns
-  # all not null values
-  # all datatype-object,object dtype is for all text data; other dtypes-int,float,date-time
   df.info()
-
-  # t1-no null values
-  # t2-all data should be in numeric format only
 
   # label encoding for single column
-  df["class"].value_counts() # eg-in column class all "e" and "p" are replaced with numeric format & total=8124
-
   print(df["class"].value_counts())
 
-  #Fetch features of type object
-  objfeatures = df.select_dtypes(include="object").columns
-  print(objfeatures)
+  # Fetch features
+  features = df.select_dtypes(include="object").columns
 
-  #label encoding for entire dataset;
-  # iterate a loop for features of type object
-  le = preprocessing.LabelEncoder() # it will assign unique values for individual features i.e cols
+  # label encoding for entire dataset
+  encoder = preprocessing.LabelEncoder() 
 
-  # for loop will run for 23 cols i.e range objfeature
-  for feat in objfeatures:
-    # all "object(str) dtype" will replace by "int dtype"
-    df[feat] = le.fit_transform(df[feat].astype(str))
+  # loop over 23 cols i.e the features set
+  for feat in features:
+    # assign unique values for features i.e cols
+    df[feat] = encoder.fit_transform(df[feat].astype(str))
+
+  print(df)
+
+  # dropping class form level 1 i.e x=features
+  X = df.drop(['class', 'stalk-root'], axis = 1) 
+  Y = df['class']
+
+  (X_train, X_test, y_train, y_test) = train_test_split(X, Y, test_size=0.20, random_state=25)
+  clf = svm.SVC(C = 100, gamma = 0.001, kernel = 'rbf')
+  clf.fit(X_train, y_train)
+  predicted = clf.predict(X_test)
+
+  print("Conf. Matrix: \n", confusion_matrix(y_test,predicted))  
+  print("Accuracy: ", accuracy_score(y_test, predicted))
+  print("Recall: ", recall_score(y_test, predicted))
+  print("F1 Score:", f1_score(y_test, predicted))
   
-  df.info()
-
+  # predict and compare
+  score = clf.score(X_test, predicted)
+  print("Mean Accuracy: ", score)
   
-
-
-  # separate x and y i.e features and labels
-  # x & y
-  X = df.drop(["class"],axis=1) # dropping class form level 1 i.e x=features
-  y = df['class'] # explicitly for class y=labels
-
-  X.info()
-
-  from sklearn.model_selection import train_test_split
-  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42)
-
-  X_train.info() # 70%values
-  X_test.info()  # 30%values
-  
-
-  #model training
-  from sklearn.naive_bayes import GaussianNB #gaussian naive bayes
-
-  gnb = GaussianNB() # creating variable of algo and follow rules defined by algo
-
-  gnb.fit(X_train, y_train) # training empty brain i.e for given x these was y i.e understanding 70% data
-
-  
-
-  # predicting Y value
-  y_prediction = gnb.predict(X_test)
-
-  print(y_prediction)
-
-  print(y_test)
+  # pred = cross_val_predict(clf, X, Y, cv=10, n_jobs=-1)
+  # conf_mat = confusion_matrix(Y, pred)
+  # print(conf_mat)
 
 if __name__ == "__main__":
+  #tuning()
   main()
+  # cap_analysis()
+  # pop_habitat_analysis()
+  # odor_analysis()
